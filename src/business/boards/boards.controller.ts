b5e70c401db391b3ac3b3d67427daf2e370b807f
DeadRiceCake <gguggong@naver.com>
2023-05-23 16:25:52 +0900

    fix: 불필요한 주석 삭제 및 validation 조건 추가

--- src/business/boards/boards.controller.ts
+++ src/business/boards/boards.controller.ts
@@ -1,28 +1,19 @@
 import {
   Body,
   Controller,
-  Delete,
   Get,
   Param,
-  Patch,
   Post,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/createBoard.dto';
-import { UpdateBoardDto } from './dto/updateBoard.dto';
-import { BoardStatusValidationPipe } from './pipes/boardStatusValidation.pipe';
 import { Board } from './board.entity';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
-
-  // @Get('/')
-  // public getAllBoards() {
-  //   return this.boardsService.getAllBoards();
-  // }
 
   @Post('/')
   @UsePipes(ValidationPipe)
@@ -34,17 +25,4 @@ export class BoardsController {
   public getBoardById(@Param('id') id: number): Promise<Board> {
     return this.boardsService.getBoardById(id);
   }
-
-  // @Delete('/:id')
-  // public deleteBoard(@Param('id') id: string) {
-  //   return this.boardsService.deleteBoard(id);
-  // }
-
-  // @Patch('/:id/status')
-  // public updateBoardStatus(
-  //   @Param('id') id: string,
-  //   @Body(new BoardStatusValidationPipe()) updateBoardDto: UpdateBoardDto,
-  // ) {
-  //   return this.boardsService.updateBoardStatus(id, updateBoardDto);
-  // }
 }
