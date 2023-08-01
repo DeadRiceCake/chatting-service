20e4ea0a4e2f76464da74e50b1c7ad4c7be802cd
DeadRiceCake <gguggong@naver.com>
2023-08-01 18:59:53 +0900

    feat: 채팅용 web socket gateway 모듈 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,15 @@ import { AuthModule } from './business/auth/auth.module';
 import { LoggerMiddleware } from './middleware/logger.middleware';
 import { APP_FILTER } from '@nestjs/core';
 import { HttpExceptionFilter } from './filter/http-exception.filter';
+import { GatewayModule } from './gateway/gateway.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(databaseConfig), AuthModule, BoardsModule],
+  imports: [
+    TypeOrmModule.forRoot(databaseConfig),
+    AuthModule,
+    BoardsModule,
+    GatewayModule,
+  ],
   providers: [Logger, { provide: APP_FILTER, useClass: HttpExceptionFilter }],
 })
 export class AppModule implements NestModule {
---
+++ src/gateway/gateway.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { ChatGateway } from './gateway';
+
+@Module({
+  imports: [ChatGateway],
+})
+export class GatewayModule {}
