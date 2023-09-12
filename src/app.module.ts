e273492c9b5b6040fd6594caf1486d107ec242ab
DeadRiceCake <gguggong@naver.com>
2023-09-12 14:46:42 +0900

    refactor: 폴더 구조 리펙터링

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module, NestModule, MiddlewareConsumer, Logger } from '@nestjs/common';
-import { BoardsModule } from './business/boards/boards.module';
+import { BoardsModule } from './boards/boards.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 import databaseConfig from './config/database.config';
-import { AuthModule } from './business/auth/auth.module';
+import { AuthModule } from './auth/auth.module';
 import { LoggerMiddleware } from './middleware/logger.middleware';
 import { APP_FILTER } from '@nestjs/core';
 import { HttpExceptionFilter } from './filter/http-exception.filter';
